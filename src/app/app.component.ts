69b5616428065de98255a17ea05c2e093d76bd98
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2022-10-21 18:07:10 -0500

    feat: integrate contracts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Web3Service} from "./services/web3/web3.service";
 import AccountWeb3Model from "./model/account.web3.model";
+import {PaymentService} from "./services/contract/payment/payment.service";
+
 
 @Component({
     selector: 'app-root',
@@ -10,25 +12,38 @@ import AccountWeb3Model from "./model/account.web3.model";
 export class AppComponent {
 
     account: AccountWeb3Model = new AccountWeb3Model();
-    private web3js:any;
+    private web3js: any;
 
 
-    constructor(private web3: Web3Service) {
-        this.web3.connectAccount().then(async web3js => {
-            this.web3js = web3js;
+    constructor(private web3Service: Web3Service,
+                private paymentService: PaymentService) {
+        this.web3Service.isConnected().then(connected => {
+            if (!connected) {
+                this.web3Service.connectAccount().then(async web3js => {
+                    this.web3js = web3js;
+                })
+            }
+            this.web3Service.accountsObservable.subscribe(account => {
+                this.updateAccountInfo();
+            });
         })
 
-        this.web3.accountsObservable.subscribe(account => {
-            this.updateAccountInfo();
-        } );
     }
 
-   async updateAccountInfo() {
+    async updateAccountInfo() {
         const accounts = await this.web3js.eth.getAccounts();
         const weiBalance = await this.web3js.eth.getBalance(accounts[0]);
         const ethBalance = Number(this.web3js.utils.fromWei(weiBalance, 'ether'));
 
         this.account = new AccountWeb3Model().build(accounts[0], ethBalance);
+    }
+
+    nuevaTransaccion(){
+        this.paymentService.nuevaTransaccion(this.account.address, "0xc6AC2A10b84173b5ef294b8FC5e21a06237F1a72", 2);
+    }
+
+    callView(){
+        this.paymentService.verBalanceCuenta();
     }
 
 
--- src/app/services/web3/web3.service.ts
+++ src/app/services/web3/web3.service.ts
@@ -4,7 +4,6 @@ import {Subject} from 'rxjs';
 import Web3 from 'web3';
 import AccountWeb3Model from "../../model/account.web3.model";
 
-
 declare let window: any;
 
 @Injectable({
@@ -14,16 +13,16 @@ export class Web3Service {
     public accountsObservable = new Subject<AccountWeb3Model[]>();
     private accountsLoaded: AccountWeb3Model[] = [];
 
+    private contractsLoaded: Map<string, any> = new Map<string, any>();
+
     public web3js: any;
 
-    constructor(@Inject(WEB3) private web3: Web3) {
+    constructor(@Inject(WEB3) public web3: Web3) {
         window.addEventListener('load', () => {
             console.log("Observing accounts!!")
             setInterval(() => this.refreshAccounts(), 100);
         });
     }
-
-
 
     async connectAccount(): Promise<Web3> {
         if (window.ethereum) {
@@ -38,15 +37,24 @@ export class Web3Service {
         return this.web3js;
     }
 
+    public isConnected(): Promise<Boolean> {
+        return new Promise((resolve, reject) => {
+            if (this.web3js) {
+                resolve(true);
+            }
+            resolve(false);
+        })
+    }
+
     private async refreshAccounts() {
         const accs = await this.web3.eth.getAccounts();
 
         if (accs.length === 0) {
-            console.info('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
+            console.warn('No account connected');
             return;
         }
 
-        if (!this.accountsLoaded || this.accountsLoaded.length !== accs.length || this.accountsLoaded[0].address !== accs[0]) {
+        if (this.accountsLoaded.length == 0 || this.accountsLoaded.length !== accs.length || this.accountsLoaded[0].address !== accs[0]) {
             console.log('Observed new accounts!');
 
             let accountsTmp: AccountWeb3Model[] = [];
@@ -59,10 +67,31 @@ export class Web3Service {
             this.accountsLoaded = accountsTmp;
 
             this.accountsObservable.next(this.accountsLoaded);
-            console.log(this.accountsLoaded)
+            console.log(this.accountsLoaded);
         }
     }
 
+    public loadContract(identifier: string, abiContractJson: any): void {
+        this.isConnected().then(async connected => {
+            if (connected) {
+                const netId = await this.web3js.eth.net.getId();
+                const deployedNetwork = abiContractJson.networks[netId];
+                const contract = new this.web3js.eth.Contract(abiContractJson.abi, deployedNetwork.address);
+                contract.setProvider(this.web3js.currentProvider);
+                this.contractsLoaded.set(identifier, contract);
+            } else {
+                console.error('Unable to load contract because it is not connected')
+            }
+        })
+
+    }
+
+    public getContract(identifier: string): any {
+        if (this.contractsLoaded.size < 1) {
+            throw Error("No contracts loaded");
+        }
+        return this.contractsLoaded.get(identifier);
+    }
 
 }
 
